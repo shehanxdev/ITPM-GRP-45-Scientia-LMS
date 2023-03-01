7c7620dcac4a59414f6dca0c4ea2e13e973ebe61
Shehan Chanuka <shehanxdev@gmail.com>
2023-03-01 09:08:42 +0530

    Theme setting has been configuared

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import LoginPage from "./layouts/LoginPage/LoginPage";
+//THEME CONFIGAURATION
 import { ThemeProvider } from "@mui/system";
 import themeSetting from "./theme";
 import { createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
+//CUSTOM CSS
+import "./styles/custom.global.css";
+
 function App() {
   const theme = createTheme(themeSetting());
   console.log(theme);
--- client/src/layouts/LoginPage/LoginPage.jsx
+++ client/src/layouts/LoginPage/LoginPage.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { AppBar, Box } from "@mui/material";
-
+//IMAGES AND ICONS
+import LOGOW from "../../assets/logo/LogoW.png";
+import SignInForm from "../../components/Shared/SignInForm";
 export default function LoginPage() {
   return (
-    <div>
-      <Box>
-        <AppBar>AppBAr</AppBar>
-      </Box>
-      <div className="row">
-        <div className="col-md-6 custom-login-form-wrapper">Login form</div>
-        <div className="col-md-6 custom-login-image-wrapper">Illustration</div>
-      </div>
+    <div className="h-100">
+      <AppBar position="static">
+        <img
+          style={{ widows: "100px", height: "100px", margin: "auto" }}
+          src={LOGOW}
+          alt=""
+        />
+      </AppBar>
+      <SignInForm />
     </div>
   );
 }
