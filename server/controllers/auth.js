012aca2c9ae118098c4d9b07ec69a13261e7f5b9
Shehan Chanuka <shehanxdev@gmail.com>
2023-04-07 21:56:54 +0530

    feat: added login routes and controllers with and incomplete model

---
+++ server/controllers/auth.js
@@ -0,0 +1,51 @@
+import Jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import User from "./../models/User.js";
+
+//REGISTERING CONTROLLER
+export const register = async (req, res) => {
+  try {
+    const {
+      //TODO implement model relted fields once databse is completed
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log(hashedPassword);
+    const newUser = new User({
+      //TODO implement model relted fields once databse is completed
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+//LOGIN IN CONTROLLER
+export const login = async (req, res) => {
+  try {
+    const { password, email } = req.body;
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(400).json({ msg: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    console.log(isMatch);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    const token = Jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "20d",
+    });
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
